d6ddfa6899b77dda628f8657bd87b92a40f334c5
Ajgar Auto Parts <ajgarautoparts@gmail.com>
2026-02-22 13:12:51 +0530

    Refactor Firebase auth and add menu functionality

--- script.js
+++ script.js
@@ -1,11 +1,49 @@
-/* ========= FIREBASE AUTH HEADER CONTROL ========= */
+/* ================= MENU TOGGLE ================= */
+const showMenu = (toggleId, navId) =>{
+  const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId);
 
+  if(toggle && nav){
+    toggle.addEventListener('click', () =>{
+      nav.classList.toggle('show-menu');
+      toggle.classList.toggle('show-icon');
+    });
+  }
+};
+showMenu('nav-toggle','nav-menu');
+
+/* ================= DROPDOWN ================= */
+const dropdownItems = document.querySelectorAll('.dropdown__item');
+
+dropdownItems.forEach((item) =>{
+  const dropdownButton = item.querySelector('.dropdown__button');
+
+  dropdownButton.addEventListener('click', () =>{
+    const showDropdown = document.querySelector('.show-dropdown');
+    toggleItem(item);
+
+    if(showDropdown && showDropdown !== item){
+      toggleItem(showDropdown);
+    }
+  });
+});
+
+const toggleItem = (item) =>{
+  const dropdownContainer = item.querySelector('.dropdown__container');
+
+  if(item.classList.contains('show-dropdown')){
+    dropdownContainer.removeAttribute('style');
+    item.classList.remove('show-dropdown');
+  } else{
+    dropdownContainer.style.height = dropdownContainer.scrollHeight + 'px';
+    item.classList.add('show-dropdown');
+  }
+};
+
+/* ================= FIREBASE AUTH ================= */
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import {
-  getAuth,
-  onAuthStateChanged,
-  signOut
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getAuth, onAuthStateChanged, signOut } 
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCvJ1h4CYZFr9h20O5zaxN6eoJtsQQypqs",
@@ -23,18 +61,15 @@ const userStatus = document.getElementById("user-status");
 const loginLink = document.getElementById("loginLink");
 const logoutBtn = document.getElementById("logoutBtn");
 
-/* ===== AUTH STATE ===== */
+/* ===== LOGIN STATE ===== */
 onAuthStateChanged(auth, (user) => {
   if (user) {
-    // LOGIN SUCCESS
     userStatus.innerText = user.email;
     loginLink.style.display = "none";
     logoutBtn.style.display = "inline-block";
 
-    // save for cart
     localStorage.setItem("loggedUser", user.email);
   } else {
-    // LOGOUT
     userStatus.innerText = "";
     loginLink.style.display = "inline-block";
     logoutBtn.style.display = "none";
